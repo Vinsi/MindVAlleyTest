be04719480d5aece8fcb44407e21505cbe03e56c
vinsi <leo.winc@gmail.com>
2017-06-06 11:37:25 +0530

    Initial Commit

---
+++ testimage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testimage
+//
+//  Created by vinsi on 6/6/17.
+//  Copyright © 2017 vinsi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ testimage/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  testimage
+//
+//  Created by vinsi on 6/6/17.
+//  Copyright © 2017 vinsi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
